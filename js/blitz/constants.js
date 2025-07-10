0fd43d2501c1d32a7557c0aea7d7b7150233552b
aleontiev <alonetiev@gmail.com>
2025-07-09 21:32:17 -0400

    ...

--- js/blitz/constants.js
+++ js/blitz/constants.js
@@ -29,7 +29,7 @@ export const COLORS = {
 export const GAME_CONFIG = {
     // Player settings
     PLAYER_SPEED: 6,
-    PLAYER_SIZE: 11,
+    PLAYER_SIZE: 10,
     PLAYER_HITBOX: 3,
     DASH_DISTANCE: 150,
     DASH_FRAMES: 40,
